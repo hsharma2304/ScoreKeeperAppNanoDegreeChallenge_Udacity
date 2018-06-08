85a1a8e93f7bea6028d6ab092e7060c79f72a588
Himani Sharma <himanisharma2304@gmail.com>
2018-06-08 11:00:34 +0530

    --ScoreKeeperApp

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -6,8 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int ScoreAllianceA = 0;
-    int ScoreAllianceB = 0;
+    public int ScoreAllianceA;
+    public int ScoreAllianceB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,31 +16,43 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Adding the score Value by 1 for Team AllianceA.
+     */
+
     public void addMainAttackA(View v) {
         ScoreAllianceA = ScoreAllianceA + 1;
         displayForTeamAllianceA(ScoreAllianceA);
 
 
     }
-
+    /**
+     * Adding the score Value by 2  for Team AllianceA.
+     */
     public void addSpecialAttackA(View v) {
         ScoreAllianceA = ScoreAllianceA + 2;
         displayForTeamAllianceA(ScoreAllianceA);
 
     }
-
+    /**
+     * Adding the score Value by 1 for Team AllianceB.
+     */
     public void addMainAttackB(View v) {
         ScoreAllianceB = ScoreAllianceB + 1;
-        displayForTeamAllianceA(ScoreAllianceA);
+        displayForTeamAllianceB(ScoreAllianceB);
 
     }
-
+    /**
+     * Adding the score Value by 2 for Team AllianceA.
+     */
     public void addSpecialAttackB(View v) {
         ScoreAllianceB = ScoreAllianceB + 2;
-        displayForTeamAllianceA(ScoreAllianceA);
+        displayForTeamAllianceB(ScoreAllianceB);
 
     }
-
+    /**
+     * Display the resetScore Button.
+     */
     public void resetScore(View v) {
         ScoreAllianceA = 0;
         ScoreAllianceB = 0;
@@ -52,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Team AllianceA.
      */
     public void displayForTeamAllianceA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.alliance_a_score);
+        TextView scoreView = findViewById(R.id.alliance_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -60,8 +72,27 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Team AllianceB.
      */
     public void displayForTeamAllianceB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.alliance_b_score);
+        TextView scoreView =  findViewById(R.id.alliance_b_score);
         scoreView.setText(String.valueOf(score));
     }
-
+    /**
+     * This code is to save the ScoreValue.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("teamA",ScoreAllianceA);
+        outState.putInt("teamB",ScoreAllianceB);
+    }
+    /**
+     * This code is to restore the value.
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        ScoreAllianceA = savedInstanceState.getInt("teamA");
+        ScoreAllianceB = savedInstanceState.getInt("teamB");
+        displayForTeamAllianceA(ScoreAllianceA);
+        displayForTeamAllianceB(ScoreAllianceB);
+    }
 }
