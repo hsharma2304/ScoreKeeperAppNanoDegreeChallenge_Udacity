a5d570c89acf50ca83e28fdb739d27aeda144053
Himani Sharma <himanisharma2304@gmail.com>
2018-06-09 15:53:43 +0530

    --ScoreKeeperApp

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -5,94 +5,98 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
-    public int ScoreAllianceA;
-    public int ScoreAllianceB;
+    public int scoreAllianceA;
+    public int scoreAllianceB;
+    TextView scoreViewB;
+    TextView scoreViewA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreViewB = findViewById(R.id.alliance_b_score);
+        scoreViewA = findViewById(R.id.alliance_a_score);
 
     }
 
     /**
      * Adding the score Value by 1 for Team AllianceA.
      */
-
     public void addMainAttackA(View v) {
-        ScoreAllianceA = ScoreAllianceA + 1;
-        displayForTeamAllianceA(ScoreAllianceA);
-
-
+        scoreAllianceA++;
+        displayForTeamAllianceA(scoreAllianceA);
     }
+
     /**
      * Adding the score Value by 2  for Team AllianceA.
      */
     public void addSpecialAttackA(View v) {
-        ScoreAllianceA = ScoreAllianceA + 2;
-        displayForTeamAllianceA(ScoreAllianceA);
-
+        scoreAllianceA += 2;
+        displayForTeamAllianceA(scoreAllianceA);
     }
+
     /**
      * Adding the score Value by 1 for Team AllianceB.
      */
     public void addMainAttackB(View v) {
-        ScoreAllianceB = ScoreAllianceB + 1;
-        displayForTeamAllianceB(ScoreAllianceB);
-
+        scoreAllianceB++;
+        displayForTeamAllianceB(scoreAllianceB);
     }
+
     /**
      * Adding the score Value by 2 for Team AllianceA.
      */
     public void addSpecialAttackB(View v) {
-        ScoreAllianceB = ScoreAllianceB + 2;
-        displayForTeamAllianceB(ScoreAllianceB);
-
+        scoreAllianceB += 2;
+        displayForTeamAllianceB(scoreAllianceB);
     }
+
     /**
      * Display the resetScore Button.
      */
     public void resetScore(View v) {
-        ScoreAllianceA = 0;
-        ScoreAllianceB = 0;
-        displayForTeamAllianceA(ScoreAllianceA);
-        displayForTeamAllianceB(ScoreAllianceB);
+        scoreAllianceA = 0;
+        scoreAllianceB = 0;
+        displayForTeamAllianceA(scoreAllianceA);
+        displayForTeamAllianceB(scoreAllianceB);
     }
 
     /**
      * Displays the given score for Team AllianceA.
      */
     public void displayForTeamAllianceA(int score) {
-        TextView scoreView = findViewById(R.id.alliance_a_score);
-        scoreView.setText(String.valueOf(score));
+
+        scoreViewA.setText(String.valueOf(score));
     }
 
     /**
      * Displays the given score for Team AllianceB.
      */
     public void displayForTeamAllianceB(int score) {
-        TextView scoreView =  findViewById(R.id.alliance_b_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewB.setText(String.valueOf(score));
     }
+
     /**
      * This code is to save the ScoreValue.
      */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("teamA",ScoreAllianceA);
-        outState.putInt("teamB",ScoreAllianceB);
+        outState.putInt("teamA", scoreAllianceA);
+        outState.putInt("teamB", scoreAllianceB);
     }
+
     /**
      * This code is to restore the value.
      */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        ScoreAllianceA = savedInstanceState.getInt("teamA");
-        ScoreAllianceB = savedInstanceState.getInt("teamB");
-        displayForTeamAllianceA(ScoreAllianceA);
-        displayForTeamAllianceB(ScoreAllianceB);
+        scoreAllianceA = savedInstanceState.getInt("teamA");
+        scoreAllianceB = savedInstanceState.getInt("teamB");
+        displayForTeamAllianceA(scoreAllianceA);
+        displayForTeamAllianceB(scoreAllianceB);
     }
 }
